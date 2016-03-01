9d78805cb7a6a584ec03b242e86e2cae90c3cfc1
Nicholas Wang <Nicholas Wang>
2016-03-01 12:45:25 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="utf-8" />
+    <meta name="robots" content="all" />
+    <meta name="author" content="Nicholas Wang" />
+    <title>Welcome to ssMyAdmin</title>
+</head>
+<style>
+
+
+</style>
+
+<body>
+    <h1 style="font-family: ">ssMyAdmin</h1>
+</body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicholas
+ * Date: 2016/3/1
+ * Time: 12:22
+ */
+?>
