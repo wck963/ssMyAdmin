d01f56bb267684918f3ae1aad37f714664fd8a8e
Nicholas <nickwck@qq.com>
2016-03-01 12:59:46 +0800

    initial building

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="zh-cn">
-<head>
+<link href='https://fonts.googleapis.com/css?family=Orbitron:500' rel='stylesheet' type='text/css'>
+<head style="width:100%;height:100%">
     <meta charset="utf-8" />
     <meta name="robots" content="all" />
     <meta name="author" content="Nicholas Wang" />
@@ -11,8 +12,9 @@
 
 </style>
 
-<body>
-    <h1 style="font-family: ">ssMyAdmin</h1>
+<body style="background-color: #d3f4ff;">
+    <h1 align="center" style="font-family: 'Orbitron',sans-serif;color: #01445b;text-shadow: 2px 2px 2px #002b3a;">ssMyAdmin</h1>
+
 </body>
 
 <?php
