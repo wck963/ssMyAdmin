06a7c40d6b1f34d54e5662f0203e500cdef691ea
Nicholas <nickwck@qq.com>
2016-03-07 23:46:50 +0800

    20160307:Updated Login button

--- auth.php
+++ auth.php
@@ -1,9 +1,15 @@
 <?php
-
-?>
+    $uname=$_GET['username'];
+    $pswd=$_GET['password'];
+    echo $uname."//////".$pswd;
+    //$state=mysql_connect('127.0.0.1','root','112526Nick');
+    echo $state;
 /**
  * Created by PhpStorm.
  * User: Nicholas
  * Date: 2016/3/1
  * Time: 18:10
  */
+
+
+?>
